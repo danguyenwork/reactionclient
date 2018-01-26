4770a59b41b95ee58dd6f35f90b803804eef2c45
Anh Nguyen <adnguyen1989@gmail.com>
2018-01-25 22:19:37 -0800

    first commit

---
+++ db/migrate/20180124175223_create_events.rb
@@ -0,0 +1,14 @@
+class CreateEvents < ActiveRecord::Migration[5.0]
+  def change
+    create_table :events do |t|
+      t.string :name
+      t.string :keywords
+      t.string :hashtags
+      t.string :language
+      t.datetime :start_time
+      t.datetime :end_time
+      t.string :img_url
+      t.timestamps
+    end
+  end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,12 @@
+# This file should contain all the record creation needed to seed the database with its default values.", "# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).", "#", "# Examples:", "#", "#   movies = Movie.create([{ name: 'Star Wars' }", "{ name: 'Lord of the Rings' }])", "#   Character.create(name: 'Luke'", "movie: movies.first)", "
+
+brands = ["Stella Artois", "Bud Light", "Michelob Ultra", "Budweiser", "Avocados From Mexico", "Coca-cola", "Doritos",  "Mountain Dew", "Pepsi", "Febreze", "Groupon", "Hulu", "Hyundai", "Turbotax", "Intuit", "Kia", "Kraft", "Lexus", "Toyota Olympics", "Toyota TBD", "M&M", "AirLinks", "Persil ProClean", "Pringles", "Squarespace", "Verizon", "WeatherTech"]
+
+search = ["Stella Artois", "Bud Light", "Michelob Ultra", "Budweiser", "Avocados From Mexico", "(Coca-Cola OR Coke)", "Doritos", "Mountain Dew OR Mtn Dew", "Pepsi", "Febreze", "Groupon", "Hulu", "Hyundai", "Turbotax", "Intuit", "Kia", "Kraft", "Lexus", "(Olympics OR Paralympics)", "TBD", "M%26M", "AirLinks", "Persil ProClean", "Pringles", "Squarespace", "Verizon", "WeatherTech"]
+
+img_name = ["stella_artois.png", "bud_light.png", "michelob_ultra.png", "budweiser.png", "avocados_from_mexico.png", "coca_cola.png", "doritos.png", "mountain_dew.png", "pepsi.png", "febreze.png", "Groupon.png", "Hulu.png", "Hyundai.png", "Turbotax.png", "Intuit.png", "Kia.png", "Kraft.png", "Lexus.png", "toyota_olympics.png", "toyota_tbd.png", "m_m.png", "AirLinks.png", "persil_proclean.png", "Pringles.png", "Squarespace.png", "Verizon.png", "WeatherTech.png"]
+
+
+brands.each_with_index  {|brand, index|
+  Event.create(name: brand, keywords: search[index], img_url: img_name[index])
+}
