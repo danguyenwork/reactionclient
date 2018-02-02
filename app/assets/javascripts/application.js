d3d13e70d30415486d67c856e63995a7f54ebbfc
Erik Dao <erikddao@gmail.com>
2018-02-03 00:45:05 +0700

    Fetch data from test.json

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,84 @@
 //= require turbolinks
 //= require_tree .
 //= require Chart.min
+
+$(document).ready(function() {
+    // Call ajax to get the data for the chart
+    var getRemoteData = function() {
+        $.ajax({
+            url: "/test.json",
+        }).success(function(data) {
+            updateSummaryStats(data);
+        }).fail(function(data) {
+        });
+    };
+
+    var updateSummaryStats = function(data) {
+        $("#sentiment_score").text(data.sentiment_score);
+        $("#num_tweets").text(data.num_tweets);
+        $("#num_retweets").text(data.num_retweets);
+    }
+
+    getRemoteData();
+});
+{/* var ctx = document.getElementById("myChart").getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'bubble',
+    data: {
+    datasets: data_points
+    },
+    options: {
+    legend: {
+        display: false
+        },
+    title: {
+        display: true,
+        text: 'Tweets clusters'
+    },
+    scales: {
+        yAxes: [{
+        scaleLabel: {
+            display: false,
+            labelString: "Sentiment"
+        },
+        ticks: {
+            display: false
+        }
+        }],
+        xAxes: [{
+        scaleLabel: {
+            display: true,
+            labelString: "Sentiment"
+        }
+        }]
+    },
+    responsive: true,
+    maintainAspectRatio: false,
+    onClick: function(e) {
+        var elements = this.getElementAtEvent(e);
+
+
+        // If you click on at least 1 elements ...
+        if (elements.length > 0) {
+            // Logs it
+            // Here we get the data linked to the clicked bubble ...
+            element = elements[0]
+            console.log(element);
+            element.custom = element.custom || {};
+            element.custom.backgroundColor = "#333333";
+
+            var datasetID = this.config.data.datasets[elements[0]._datasetIndex].id;
+            // data gives you `x`, `y` and `r` values
+            // var data = this.config.data.datasets[elements[0]._datasetIndex].data[elements[0]._index];
+
+            var cluster_divs = $(document.querySelectorAll("[data-tag]"))
+            cluster_divs.each(function(){
+                $(this).addClass('hidden');
+                if ($(this).data('tag') == datasetID) {
+                    $(this).removeClass('hidden')
+                }
+            })
+        }
+    }
+    }
+}); */}
