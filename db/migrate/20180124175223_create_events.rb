1246b26c19548e79ea23bd439eb30af8292d31e6
Anh Nguyen <adnguyen1989@gmail.com>
2018-02-03 15:56:19 -0800

    api

--- db/migrate/20180124175223_create_events.rb
+++ db/migrate/20180124175223_create_events.rb
@@ -8,6 +8,7 @@ class CreateEvents < ActiveRecord::Migration[5.0]
       t.datetime :start_time
       t.datetime :end_time
       t.string :img_url
+      t.string :db_name
       t.timestamps
     end
   end
